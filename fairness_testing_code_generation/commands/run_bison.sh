073848cfcbfa3631f2693743df328a472a41153f
Philly <lfilippelli5@gmail.com>
2026-03-09 16:36:53 -0400

    Clone Bias Unveiled repository

--- fairness_testing_code_generation/commands/run_bison.sh
+++
@@ -1,4 +0,0 @@
-## BISON
-bash exp_by_hyp_batch.sh bison 5 default
-bash exp_by_style_batch.sh bison 5 1.0
-bash exp_by_iterations.sh bison 3 5
--- fairness_testing_code_generation/commands/run_claude.sh
+++
@@ -1,4 +0,0 @@
-## CLAUDE
-bash exp_by_hyp_batch.sh claude 5 default
-bash exp_by_style_batch.sh claude 5 1.0
-bash exp_by_iterations.sh claude 3 5
